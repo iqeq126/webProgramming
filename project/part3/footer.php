73283ec5237851eb5389898511911e9c10d6bf10
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2023-12-02 20:31:01 +0900

    일부 정보 수정

--- project/part3/footer.php
+++ project/part3/footer.php
@@ -2,11 +2,11 @@
             <p id="footer_logo">오늘 뭐하지? | <span>한국기술교육대학교</span></p>
             <ul id="download">
                 <li>웹페이지 작성자 소속 및 이름</li>
-                <li>- 한국기술교육대학교 2018136121</li>
-                <li>- 조원석 : 010-8024-1320 </li>
+                <li>- 이름</li>
+                <li>- 번호 </li>
             </ul>
             <ul id="author">
                 <li>문의 메일</li>
-                <li>- 메일 주소 : iqeq126@koreatech.ac.kr</li>
+                <li>- 메일 주소</li>
             </ul>
         </div>
